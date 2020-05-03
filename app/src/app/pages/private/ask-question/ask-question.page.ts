175206896ebc71789d6644e293ccbbec2f52a87d
Hasan Can Özkan <ben@canozkan.net>
2020-05-03 23:15:37 +0300

    ionic: add question

--- app/src/app/pages/private/ask-question/ask-question.page.ts
+++ app/src/app/pages/private/ask-question/ask-question.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
- 
+import { HttpClient } from '@angular/common/http';
+import { ConstantService } from '../../../services/constant/constant.service'
+import { Tag } from '../../../models/tag.model';
+import { AuthenticationService } from '../../../services/authenticaton/authentication.service';
+import { QuestionService } from '../../../services/question/question.service';
+import { ToastService } from '../../../services/toast/toast.service';
 @Component({
   selector: 'app-ask-question',
   templateUrl: './ask-question.page.html',
@@ -7,29 +12,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskQuestionPage implements OnInit {
 
-  htmlText =""
+  postData = {
+    title: "",
+    content: "",
+    tags: null,
+  }
 
   // auto complete fields
-  autocompleteFields = [
-    {
-      display: 'Javascript'
-    },
-    {
-      display: 'Java'
-    },
-    {
-      display: 'React'
-    },
-  ]
+  autocompleteFields = []
 
-  constructor() { }
+  constructor(
+    private http : HttpClient,
+    private constantService : ConstantService,
+    private toast : ToastService,
+    private auth : AuthenticationService,
+    private qService : QuestionService) { }
 
   ngOnInit() {
-
+    this.http.get<Tag[]>(this.constantService.API_URL + 'tags').subscribe(res => {    
+      for(let i=0; i < res.length; i++)
+      {
+        this.autocompleteFields.push({display: res[i].TagName, value: res[i].TagID})
+      }
+    })
   }
 
-  deneme() {
-    console.log(this.htmlText);
+  // check inputs
+  checkFields(): boolean
+  {
+    // set clear variable
+    let title = this.postData.title.trim();
+    let content = this.postData.content.trim();
+    let tags = this.postData.tags;
+    // check blanks
+    if(
+      title == "" ||
+      content == "" ||
+      tags == null
+    )
+    {
+      this.toast.warning("Boş Alan Bırakmayınız");
+      return false;
+    }
+
+    return true;
   }
 
+  // add question
+  addQuestion() {
+    if(this.checkFields())
+    {
+      this.auth.userData$.subscribe(res => {
+        this.qService.addQuestion(res.data, JSON.stringify(this.postData)).subscribe(response => {
+          if(response.message == "ADD_QUESTION_SUCCESS")
+          {
+            this.toast.success("Sorunuz başarı ile eklenmiştir!");
+            this.clear();
+            // TODO : burada soru yönlendirmesi olacak.
+          }
+
+          if(response.message == "ADD_QUESTION_FAILED")
+          {
+            this.toast.error("İşlem başarısız!");
+          }
+
+          if(response.message == "AUTHORIZATION_FAILED")
+          {
+            this.auth.logout();
+          }
+        })
+      })
+    }
+  }
+
+  // clear inputs
+  clear()
+  {
+    this.postData.title = "";
+    this.postData.content = "";
+    this.postData.tags = null;
+  }
 }
---
+++ app/src/app/services/question/question.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ConstantService } from '../../services/constant/constant.service'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor(
+    private constantService: ConstantService,
+    private http: HttpClient,
+  ) { }
+
+  // Add Question
+  addQuestion(auth, post_data) : Observable<any>
+  {
+    return this.http.post(this.constantService.API_URL + 'question/add', post_data, {
+      headers: {'Authorization': 'UserID='+ auth.UserID +'; token='+ auth.Token +';'}
+    })
+  }
+}
